6e40bca908c84ab0aa8a8ed0fdd51dc8332847ac
SahanaSPatil <sahana17spatil@gmail.com>
2024-12-25 16:36:27 +0530

    Added checkStatus function

--- EvenOdd.py
+++ EvenOdd.py
@@ -10,8 +10,35 @@ def checkOddEven(x):
     else:
         print("Even")
         
+"""
+Given two integer variables a and b, and a boolean variable flag. The task is to check the status and return accordingly.
 
-X = int(input("Enter a number : "))
-checkOddEven(X)
+Return True for the following cases:
+
+Either a or b (not both) is non-negative and the flag is false.
+Both a and b are negative and the flag is true.
+Otherwise, return False.
+"""
+
+def checkStatus(a, b, flag):
+    if(a >= 0 and b < 0 and flag == False):
+        return True
     
+    elif(a < 0 and b >= 0 and flag == False):
+        return True
+    
+    elif(a < 0 and b < 0 and flag):
+        return True
+    
+    else:
+        return False
+    
+        
 
+#X = int(input("Enter a number : "))
+#checkOddEven(X)
+    
+a = int(input("Enter a number(a) : "))
+b = int(input("Enter a number(b) : "))
+flag = int((input("Enter 0/1 : ")))
+print(checkStatus(a , b , flag))
